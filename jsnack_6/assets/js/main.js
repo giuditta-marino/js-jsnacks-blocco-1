f4b1aa190140281c29ff343c08acb3c62a84fecb
Giuditta <giudittamarino89@gmail.com>
2021-03-03 15:37:00 +0100

    jsnack_6

---
+++ jsnack_6/assets/js/main.js
@@ -0,0 +1,7 @@
+// CHIEDO UN NUMERO ALL'UTENTE
+var n = prompt("Inserisci un numero");
+
+// PER N VOLTE PRENDO I NUMERI IN SUCCESSIONE E NE CALCOLO IL CUBO
+for (var i = 1; i <= n; i++) {
+  console.log(i*i*i);
+}
