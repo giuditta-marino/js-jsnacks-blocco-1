a9fa3e67653e955cec33c0c0a0d6bdfa5fbe5671
Giuditta <giudittamarino89@gmail.com>
2021-03-04 11:25:02 +0100

    jsnack_5 perfezionato

--- jsnack_5/assets/js/main.js
+++ jsnack_5/assets/js/main.js
@@ -4,8 +4,8 @@ var listaNumeri = [];
 
 // CHIEDO PER 6 VOLTE ALL'UTENTE DI INSERIRE UN NUMERO, POI CONTROLLO CHE IL NUMERO SIA DISPARI E SE LO E' LO INSERISCO NELL'ARRAY
 for (var i = 0; i < 6; i++) {
-  var numero = prompt ("Inserisci un numero");
-    if (numero%2) {
+  var numero = parseInt(prompt ("Inserisci un numero"));
+    if (!isNaN(numero) && numero%2) {
     listaNumeri.push(numero);
     }
   }
